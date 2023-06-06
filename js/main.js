3ac98841f8f22909492b8720033fe181c4bcec7f
nbcnvc <nbcnvc.dev@gmail.com>
2023-06-06 22:57:23 +0900

    Fixed

--- js/main.js
+++ js/main.js
@@ -1,29 +1,15 @@
 import { getTopRatedMovies } from "./fetch.js"; //영화데이터 fetch
 import { slide } from "./slide.js"; //상단부 슬라이드 동작기능
 import { toDetail } from "./home-to-detail.js"; //상세페이지 이동기능
+
+const cardContainer = document.querySelector(".card-container");
+const searchInput = document.querySelector(".search-input");
+const searchButton = document.querySelector(".search-btn");
+const popularity = document.querySelector(".popularity");
+
 const data = await getTopRatedMovies();
+
 //데이터 화면에 출력하기
-
-const makeMovieList = (data) => {
-  //div card-container (하단부분)
-  data.forEach((movie) => {
-    const cardContainer = document.querySelector(".card-container");
-    cardContainer.innerHTML += `<div data-id="${movie.id}" class="movie-card">
-      <div class="card-poster">
-        <img
-          src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
-          alt="name poster"
-        />
-      </div>
-      <div class="card-title">${movie.title}</div>
-      <div class="card-rating">${movie.vote_average}</div>
-    </div>`;
-  });
-
-  toDetail();
-};
-makeMovieList(data);
-
 //slide (상단부분)
 const makeSlideMovieList = (data) => {
   let filterdData = data.sort((a, b) => b.vote_average - a.vote_average); // 평점순으로 보여주기
@@ -48,33 +34,57 @@ const makeSlideMovieList = (data) => {
 
   slide();
 };
-makeSlideMovieList(data);
-//검색기능
-const cardContainer = document.querySelector(".card-container");
-const searchInput = document.querySelector(".search-input");
-const searchButton = document.querySelector(".search-btn");
 
-searchButton.addEventListener("click", function () {
-  const query = searchInput.value;
-  showData(query);
-});
+const makeMovieList = (data) => {
+  //div card-container (하단부분)
+  data.forEach((movie) => {
+    const cardContainer = document.querySelector(".card-container");
+    cardContainer.innerHTML += `<div data-id="${movie.id}" class="movie-card">
+      <div class="card-poster">
+        <img
+          src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+          alt="name poster"
+        />
+      </div>
+      <div class="card-title">${movie.title}</div>
+      <div class="card-rating">${movie.vote_average}</div>
+    </div>`;
+  });
+
+  toDetail();
+};
+
 const showData = async (query) => {
   const data = await getTopRatedMovies();
   const inputData = query.toLowerCase();
   const filteredData = data.filter((d) =>
     d.title.toLowerCase().includes(inputData)
   );
+
   cardContainer.innerHTML = "";
   makeMovieList(filteredData);
 };
+
+// entrypoint
+makeSlideMovieList(data);
+makeMovieList(data);
+
+
+
+// event listener
+//검색기능
+searchButton.addEventListener("click", () => {
+  const query = searchInput.value;
+  showData(query);
+});
+
 //정렬기능
-const popularity = document.querySelector(".popularity");
 popularity.addEventListener("click", () => {
   const dataSortedByPopularity = data.sort(
     (a, b) => b.popularity - a.popularity
   );
+
   cardContainer.innerHTML = "";
   makeMovieList(dataSortedByPopularity);
 });
-// const vote = document.querySelector('.vote')
-// const comment = document.querySelector('.comment')
+
